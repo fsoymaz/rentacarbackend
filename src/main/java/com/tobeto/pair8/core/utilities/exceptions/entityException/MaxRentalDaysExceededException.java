d3a0397671e7d8acdba0b1f1c09acd3f3a87c22e
fatih soymaz <fatihsoymaz@fatih-MacBook-Air.local>
2024-02-20 10:42:29 -0800

    .

--- src/main/java/com/tobeto/pair8/core/utilities/exceptions/entityException/MaxRentalDaysExceededException.java
+++ src/main/java/com/tobeto/pair8/core/utilities/exceptions/entityException/MaxRentalDaysExceededException.java
@@ -1,4 +1,4 @@
-package com.tobeto.pair8.core.utilities.exceptions;
+package com.tobeto.pair8.core.utilities.exceptions.entityException;
 
 public class MaxRentalDaysExceededException extends RuntimeException {
     public MaxRentalDaysExceededException(String message) {
