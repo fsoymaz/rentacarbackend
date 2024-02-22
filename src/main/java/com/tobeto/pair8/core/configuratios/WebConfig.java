b0886890061ea7d705a0a7470ac8324c96a3c4a1
fatih soymaz <fatihsoymaz@fatih-MacBook-Air.local>
2024-02-22 20:24:07 +0300

    .

--- src/main/java/com/tobeto/pair8/core/configuratios/WebConfig.java
+++ src/main/java/com/tobeto/pair8/core/configuratios/WebConfig.java
@@ -5,9 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowedHeaders("*");
+        registry.addMapping("/*")
+                .allowedOrigins("")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization");
     }
+
 }
